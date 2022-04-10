d3669dd79d741cb7f46f61b9cbeb63e06ea8eb3f
fadilfauzani <16520180@mahasiswa.itb.ac.id>
2022-04-11 01:22:09 +0700

    change character extends card

--- src/main/java/com/aetherwars/model/Character.java
+++ src/main/java/com/aetherwars/model/Character.java
@@ -1,24 +1,33 @@
 package com.aetherwars.model;
+import com.aetherwars.model.Card;
 
-public class Character {
-  private String name;
-  private String description;
-  private Type type;
-
-  public Character() {
-    this.name = "";
-    this.description = "";
-    this.type = Type.OVERWORLD;
+public class Character extends Card{
+  private int attack;
+  private int health;
+  public Character(){
+    super();
+    this.attack = 0;
+    this.health = 0;
   }
-
-  public Character(String name, String description, Type element) {
-    this.name = name;
-    this.description = description;
-    this.type = element;
+  public Character(String name, String description, Type type, int attack, int health){
+    super(name, description, type);
+    this.attack = attack;
+    this.health = health;
   }
-
+  public void setAttack(int newAttack){
+    this.attack = newAttack;
+  }
+  public int getAttack(){
+    return this.attack;
+  }
+  public void setHealth(int newHealth){
+    this.health = newHealth;
+  }
+  public int getHealth(){
+    return this.health;
+  }
   @Override
   public String toString() {
-    return "Name: " + this.name + "\nDescription: " + this.description + "\nType: " + this.type;
+    return super.toString() + "\nAttack: " + this.attack + "\nHealth: " + this.health;
   }
 }
