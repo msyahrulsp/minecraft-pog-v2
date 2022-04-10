b5d7fcbc9805c706e35770b02fb5a5054183197d
fadilfauzani <16520180@mahasiswa.itb.ac.id>
2022-04-11 01:40:46 +0700

    add lvl system on character card

--- src/main/java/com/aetherwars/model/Character.java
+++ src/main/java/com/aetherwars/model/Character.java
@@ -3,16 +3,28 @@ import com.aetherwars.model.Card;
 
 public class Character extends Card{
   private int attack;
+  private int attUp;
   private int health;
+  private int healthUp;
+  private int exp;
+  private int lvl;
   public Character(){
     super();
     this.attack = 0;
+    this.attUp = 1;
     this.health = 0;
+    this.healthUp = 1;
+    this.exp = 0;
+    this.lvl = 1;
   }
-  public Character(String name, String description, Type type, int attack, int health){
+  public Character(String name, String description, Type type, int attack, int health, int attUp, int healthUp){
     super(name, description, type);
     this.attack = attack;
+    this.attUp = attUp;
     this.health = health;
+    this.healthUp = healthUp;
+    this.exp = 0;
+    this.lvl = 1;
   }
   public void setAttack(int newAttack){
     this.attack = newAttack;
@@ -25,6 +37,32 @@ public class Character extends Card{
   }
   public int getHealth(){
     return this.health;
+  }
+  private void handleLevelup(){
+    int lvlxpcap = this.lvl * 2 - 1;
+    while (this.exp >= lvlxpcap && this.lvl < 10){
+      this.attack += this.attUp;
+      this.health += this.healthUp;
+      this.lvl += 1;
+      this.exp -= lvlxpcap;
+      lvlxpcap = this.lvl * 2 - 1;
+    }
+    if (this.lvl == 10) this.exp = 0;
+  }
+  public void addExp(int expAmount){
+    if (this.lvl < 10){
+      this.exp += xpAmount;
+      handleLevelup();
+    }
+  }
+  public int getExp(){
+    return this.Exp;
+  }
+  public void setLvl(int newLvl){
+    this.lvl = newLvl;
+  }
+  public int getLvl(){
+    return this.lvl;
   }
   @Override
   public String toString() {
