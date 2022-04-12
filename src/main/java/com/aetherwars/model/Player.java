3629dcaed62d3c44ed81726aa4a643a66d32b861
ranjabi <fikriranjabi02@gmail.com>
2022-04-12 20:52:20 +0700

    add load character to player deck

--- src/main/java/com/aetherwars/model/Player.java
+++ src/main/java/com/aetherwars/model/Player.java
@@ -1,20 +1,27 @@
 package com.aetherwars.model;
 import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import com.aetherwars.util.CSVReader;
 
 public class Player<T> {
+    private static final String CHARACTER_CSV_FILE_PATH = "../card/data/character.csv";
     private String name;
     private Integer health;
     private Integer mana;
-    private ArrayList<T> deck;
+    private ArrayList<Card> deck;
+    private ArrayList<T> board;
+    private ArrayList<T> hand;
 
     public Player() {
         this.name = "";
-        this.health = 0;
+        this.health = 80;
         this.mana = 0;
-        this.deck = new ArrayList<T>();
+        this.deck = new ArrayList<Card>();
     }
 
-    public Player(String name, Integer health, Integer mana, ArrayList<T> card) {
+    public Player(String name, Integer health, Integer mana, ArrayList<Card> card) {
         this.name = name;
         this.health = health;
         this.mana = mana;
@@ -42,31 +49,63 @@ public class Player<T> {
     }
 
     public void showDeck() {
+        System.out.print("[");
         for(int i=0; i<this.deck.size(); i++) {
-            System.out.println(deck.get(i).toString());
+            if (i+1 != this.deck.size()) {
+                System.out.print(deck.get(i).getName() + ",");
+            } else {
+                System.out.println(deck.get(i).getName() + "]");
+            }
         }
     }
-
+    
     @Override
     public String toString() {
-        System.out.println("Player deck list: ");
+        System.out.println("Player " + this.getName() + " deck list: ");
         this.showDeck();
         return "\nName: " + this.getName() + "\nHealth: " + this.getHealth() + "\nMana: " + this.getMana();
     }
-
+    
     public <T> void getCard() {};
     public void getCardInfo(T card) {};
-    public void putCard() {};
     public void attack() {};
     public void nextPhase() {};
 
+    public void putCardToDeck(String[] row) {
+        Type type;
+        if (row[2] == "OVERWORLD") {
+            type = Type.OVERWORLD;
+        } else if (row[2] == "NETHER") {
+            type = Type.NETHER;
+        } else {
+            type = Type.END;
+        }
+        Character temp = new Character(Integer.parseInt(row[0]), row[1], row[3], Integer.parseInt(row[7]), row[4], type, Integer.parseInt(row[5]), Integer.parseInt(row[6]), Integer.parseInt(row[8]), Integer.parseInt(row[9]));
+        this.deck.add(temp);
+    };
+    
+    public void loadDeck(List<String[]> deck) {
+        for(int i=0; i<17; i++) {
+            putCardToDeck(deck.get(i));
+        }
+    }
+
+    public List<String[]> loadCards() throws IOException, URISyntaxException {
+        File characterCSVFile = new File(getClass().getResource(CHARACTER_CSV_FILE_PATH).toURI());
+        CSVReader characterReader = new CSVReader(characterCSVFile, "\t");
+        characterReader.setSkipHeader(true);
+        List<String[]> characterRows = characterReader.read();
+        return characterRows;
+    }
+
     public static void main(String[] args) {
-        Character d = new Character(1, "test", "test", 1, "test", Type.OVERWORLD, 1, 1, 1, 1);
-        Character e = new Character(2, "test", "test", 2, "test", Type.OVERWORLD, 1, 1, 1, 1);
         ArrayList<Character> newDeck = new ArrayList<>();
-        newDeck.add(d);
-        newDeck.add(e);
-        Player<Card> one = new Player("kevin", 80, 1, newDeck);
-        System.out.println(one.toString());
+        Player<Card> playerOne = new Player("kevin", 80, 1, newDeck);
+        try {
+            playerOne.loadDeck(playerOne.loadCards());
+        } catch (Exception err) {
+            System.out.println("Load file error");
+        }
+        System.out.println(playerOne.toString());
     }
 }
