77d42fa86f30271f55bbcc2f858e5d6b596891ae
ranjabi <fikriranjabi02@gmail.com>
2022-04-12 12:58:34 +0700

    add player tester

--- src/main/java/com/aetherwars/model/Player.java
+++ src/main/java/com/aetherwars/model/Player.java
@@ -21,9 +21,52 @@ public class Player<T> {
         this.deck = card;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getHealth() {
+        return this.health;
+    }
+
+    public Integer getMana() {
+        return this.mana;
+    }
+
+    public void setHealth(Integer health) {
+        this.health = health;
+    }
+
+    public void setMana(Integer mana) {
+        this.mana = mana;
+    }
+
+    public void showDeck() {
+        for(int i=0; i<this.deck.size(); i++) {
+            System.out.println(deck.get(i).toString());
+        }
+    }
+
+    @Override
+    public String toString() {
+        System.out.println("Player deck list: ");
+        this.showDeck();
+        return "\nName: " + this.getName() + "\nHealth: " + this.getHealth() + "\nMana: " + this.getMana();
+    }
+
     public <T> void getCard() {};
     public void getCardInfo(T card) {};
     public void putCard() {};
     public void attack() {};
     public void nextPhase() {};
+
+    public static void main(String[] args) {
+        Character d = new Character(1, "test", "test", 1, "test", Type.OVERWORLD, 1, 1, 1, 1);
+        Character e = new Character(2, "test", "test", 2, "test", Type.OVERWORLD, 1, 1, 1, 1);
+        ArrayList<Character> newDeck = new ArrayList<>();
+        newDeck.add(d);
+        newDeck.add(e);
+        Player<Card> one = new Player("kevin", 80, 1, newDeck);
+        System.out.println(one.toString());
+    }
 }
