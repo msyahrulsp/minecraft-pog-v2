9c157b344e8e101561c1ba73f198dced912f2a0f
ranjabi <fikriranjabi02@gmail.com>
2022-04-25 05:59:32 +0700

    test add base game gui to aetherwars.java

--- src/main/java/com/aetherwars/AetherWars.java
+++ src/main/java/com/aetherwars/AetherWars.java
@@ -6,8 +6,10 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -18,39 +20,33 @@ import com.aetherwars.util.CSVReader;
 public class AetherWars extends Application {
  private static final String CHARACTER_CSV_FILE_PATH = "card/data/character.csv";
 
- public void loadCards() throws IOException, URISyntaxException {
-   File characterCSVFile = new File(getClass().getResource(CHARACTER_CSV_FILE_PATH).toURI());
-   CSVReader characterReader = new CSVReader(characterCSVFile, "\t");
-   characterReader.setSkipHeader(true);
-   List<String[]> characterRows = characterReader.read();
-   for (String[] row : characterRows) {
-//      Character c = new Character(row[1], row[3], Type.valueOf(row[2]));
-     System.out.println(row);
-   }
- }
+// public void loadCards() throws IOException, URISyntaxException {
+//   File characterCSVFile = new File(getClass().getResource(CHARACTER_CSV_FILE_PATH).toURI());
+//   CSVReader characterReader = new CSVReader(characterCSVFile, "\t");
+//   characterReader.setSkipHeader(true);
+//   List<String[]> characterRows = characterReader.read();
+////   for (String[] row : characterRows) {
+//////      Character c = new Character(row[1], row[3], Type.valueOf(row[2]));
+////     System.out.println(row);
+////   }
+// }
 
  @Override
- public void start(Stage stage) {
-   Text text = new Text();
-   text.setText("Loading...");
-   text.setX(50);
-   text.setY(50);
+ public void start(Stage stage) throws Exception {
+   FXMLLoader gameLoader = new FXMLLoader(AetherWars.class.getResource("gui/BaseGame.fxml"));
+   BorderPane baseGame = gameLoader.load();
 
-   Group root = new Group();
-   root.getChildren().add(text);
-
-   Scene scene = new Scene(root, 1280, 720);
+   Scene scene = new Scene(baseGame, 1280, 720);
 
    stage.setTitle("Minecraft: Aether Wars");
    stage.setScene(scene);
    stage.show();
 
-   try {
-     this.loadCards();
-     text.setText("Minecraft: Aether Wars!");
-   } catch (Exception e) {
-     text.setText("Failed to load cards: " + e);
-   }
+//   try {
+//     this.loadCards();
+//   } catch (Exception e) {
+//       System.out.println(e);
+//   }
  }
 
  public static void main(String[] args) {
---
+++ src/main/java/com/aetherwars/controller/BaseGameController.java
@@ -0,0 +1,16 @@
+package com.aetherwars.controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BaseGameController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
